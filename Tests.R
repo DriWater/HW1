291a033e7d82c8a26a21f898bbd422f54fd52f41
DriWater <heshuren987@gmail.com>
2022-08-26 15:46:54 -0500

    Test functions on iris dataset

--- Tests.R
+++ Tests.R
@@ -2,3 +2,15 @@
 
 # Source the functions
 source("FunctionsLM.R")
+
+# load and process data set
+dat = iris
+dat$isSetosa <- ifelse(dat$Species == 'setosa',1,0)
+dat$isVersicolor <- ifelse(dat$Species == 'versicolor',1,0)
+dat <- dat[,-5]
+
+# Test functions on iris datasets
+Y  = dat$Petal.Length
+X = dat[,-3]
+beta_LS <- calculateBeta(X,Y)
+calculatePredictionError(Y,X,beta_LS) #3.23908
